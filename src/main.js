13f7031423d4610e6f347f0809b81182b76f69cb
chiragsheoran <cr8ter4real@gmail.com>
2022-03-11 16:55:36 +0530

    final commit

--- src/main.js
+++ src/main.js
@@ -19,11 +19,13 @@ const renderer = new THREE.WebGLRenderer({
 
 loadGLTF("../models/scene.gltf", (gltf) => {
   scene.add(gltf.scene);
-  gltf.scene.scale.set(25, 25, 25);
-  gltf.scene.position.setZ(-20);
-  gltf.scene.position.setX(-2);
+  gltf.scene.scale.set(30, 30, 30);
+  gltf.scene.position.setZ(-8);
+  gltf.scene.position.setX(-3);
   gltf.scene.position.setY(-2);
-  gltf.scene.rotation.z += 3;
+  gltf.scene.rotation.x += -0.5;
+  gltf.scene.rotation.y += -0.4;
+  gltf.scene.rotation.z += -0.3;
   gltf.animations; // Array<THREE.AnimationClip>
   gltf.scene; // THREE.Group
   gltf.scenes; // Array<THREE.Group>
@@ -33,7 +35,13 @@ loadGLTF("../models/scene.gltf", (gltf) => {
 
 loadGLTF("../models/arduino/scene.gltf", (gltf) => {
   scene.add(gltf.scene);
-  gltf.scene.position.setZ(-40);
+  gltf.scene.scale.set(10, 10, 10);
+  gltf.scene.position.setZ(-16);
+  gltf.scene.position.setX(-15);
+  gltf.scene.position.setY(-5);
+  gltf.scene.rotation.x += -2.5;
+  gltf.scene.position.z += 0.4;
+  gltf.scene.rotation.y += 0.9;
   gltf.animations; // Array<THREE.AnimationClip>
   gltf.scene; // THREE.Group
   gltf.scenes; // Array<THREE.Group>
@@ -57,7 +65,7 @@ scene.background = spaceTexture;
 // scene.add(torus)
 
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(-2, 1, -20);
+pointLight.position.set(-2, 2, -20);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 
@@ -97,7 +105,7 @@ chirag.position.setX(-10);
 const maskkTexture = new THREE.TextureLoader().load("../maskkmat.jpeg");
 
 const maskk = new THREE.Mesh(
-  new THREE.BoxGeometry(12, 12, 12),
+  new THREE.BoxGeometry(6, 6, 6),
   new THREE.MeshStandardMaterial({
     map: maskkTexture,
   })
@@ -105,13 +113,13 @@ const maskk = new THREE.Mesh(
 
 scene.add(maskk);
 
-maskk.position.setZ(-55);
-maskk.position.setX(15);
+maskk.position.setZ(-50);
+maskk.position.setX(12);
 
 const gunTexture = new THREE.TextureLoader().load("../gunmatt.jpeg");
 
 const gun = new THREE.Mesh(
-  new THREE.BoxGeometry(15, 15, 15),
+  new THREE.BoxGeometry(6, 6, 6),
   new THREE.MeshStandardMaterial({
     map: gunTexture,
   })
@@ -119,9 +127,53 @@ const gun = new THREE.Mesh(
 
 scene.add(gun);
 
-gun.position.setZ(-20);
-gun.position.setX(-15);
-gun.position.setY(15);
+const pythonTexture = new THREE.TextureLoader().load("../pythonmat.png");
+
+const python = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshStandardMaterial({
+    map: pythonTexture,
+  })
+);
+
+scene.add(python);
+
+python.position.setZ(-20);
+python.position.setX(-23);
+python.position.setY(-8);
+
+const jsTexture = new THREE.TextureLoader().load("../jsmat.png");
+
+const js = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshStandardMaterial({
+    map: jsTexture,
+  })
+);
+
+scene.add(js);
+
+js.position.setZ(-18);
+js.position.setX(-2);
+js.position.setY(-6);
+
+const kaliTexture = new THREE.TextureLoader().load("../kalimat.jpg");
+const kali = new THREE.Mesh(
+  new THREE.BoxGeometry(5, 5, 5),
+  new THREE.MeshStandardMaterial({
+    map: kaliTexture,
+  })
+);
+
+scene.add(kali);
+
+kali.position.setZ(-12);
+kali.position.setX(-14);
+kali.position.setY(5);
+
+gun.position.setZ(-55);
+gun.position.setX(13);
+gun.position.setY(-8);
 
 function animate() {
   requestAnimationFrame(animate);
@@ -130,11 +182,20 @@ function animate() {
   // torus.rotation.y += 0.005;
   // torus.rotation.z += 0.01;
 
-  gun.rotation.x += 0.01;
-  gun.rotation.y += 0.005;
+  gun.rotation.y += 0.01;
+  //gun.rotation.z += 0.002;
 
-  maskk.rotation.x += 0.01;
-  maskk.rotation.y += 0.002;
+  kali.rotation.x += 0.01;
+  kali.rotation.z += 0.002;
+
+  python.rotation.z -= 0.01;
+  python.rotation.x += 0.01;
+
+  js.rotation.y += 0.01;
+  js.rotation.x -= 0.01;
+
+  //maskk.rotation.z += 0.01;
+  maskk.rotation.y += 0.01;
 
   chirag.rotation.y += 0.005;
   chirag.rotation.z += 0.001;
